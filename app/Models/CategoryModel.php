eff584e537097b1039203c14e1d6609896aa187b
lilousir <lilousir1999@gmail.com>
2024-12-20 11:54:13 +0100

    feating: admin catégory

--- app/Models/CategoryModel.php
+++ app/Models/CategoryModel.php
@@ -18,15 +18,6 @@ class CategoryModel extends Model
     protected bool $updateOnlyChanged = true;
 
     // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 
     public function getCategoryById($id){
         return $this->find($id);
@@ -35,4 +26,84 @@ class CategoryModel extends Model
     public function getAllCategory(){
         return $this->findAll();
     }
+    public function createCategory($data){
+        $data['slug'] = $this->generateUniqueSlug($data['name']);
+
+        return $this->insert($data);
+    }
+
+    public function updateCategory($id,$data){
+        if(isset($data['name'])) {
+            $data['slug'] = $this->generateUniqueSlug($data['name'], $id);
+        }
+        return $this->update($id,$data);
+    }
+    public function deleteCategory($id)
+    {
+        return $this->delete($id);
+    }
+    public function getPaginatedCategory($start, $length, $searchValue, $orderColumnName, $orderDirection)
+    {
+        $builder = $this->builder();
+        $builder->select("category.*");
+
+        // Recherche
+        if (!empty($searchValue)) {
+            $builder->like('category.name', $searchValue);
+
+        }
+
+        // Tri
+        if ($orderColumnName && $orderDirection) {
+            $builder->orderBy($orderColumnName, $orderDirection);
+        }
+
+        $builder->limit($length, $start);
+
+        return $builder->get()->getResultArray();
+    }
+
+    // Obtenir le nombre total de cinémas
+    public function getTotalCategory()
+    {
+        return $this->countAllResults();
+    }
+
+    // Obtenir le nombre total de cinémas filtrés par recherche
+    public function getFilteredCategory($searchValue, $limit = null, $offset = null)
+    {
+        $builder = $this->builder();
+        $builder->select("category.*");
+
+        // Filtrage
+        if (!empty($searchValue)) {
+            $builder->groupStart();
+            $builder->like('category.name', $searchValue);
+            $builder->groupEnd();
+        }
+
+        // Pagination
+        if ($limit !== null) {
+            $builder->limit($limit, $offset);
+        }
+
+        return $builder->get()->getResultArray(); // Retourne les données
+    }
+    private function generateUniqueSlug($name)
+    {
+        $slug = generateSlug($name);
+        $builder = $this->builder();
+        $count = $builder->where('slug', $slug)->countAllResults();
+        if ($count === 0) {
+            return $slug;
+        }
+        $i = 1;
+        while ($count > 0) {
+            $newSlug = $slug . '-' . $i;
+            $count = $builder->where('slug', $newSlug)->countAllResults();
+            $i++;
+        }
+        return $newSlug;
+    }
+
 }
