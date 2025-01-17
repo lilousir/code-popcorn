9520b42f7aa5878092f47476c881b170921f8f86
lilousir <lilousir1999@gmail.com>
2025-01-17 12:16:02 +0100

    feat : lien film (#22)

--- app/Views/film/movies.php
+++ app/Views/film/movies.php
@@ -13,7 +13,7 @@ $controller = strtolower(basename(str_replace('\\', '/', $router->controllerName
                     <?php foreach ($movies as $movie): ?>
                         <?php
                         $title = isset($movie['title']) ? $movie['title'] : 'Titre inconnu';
-                        $slug = isset($movie['slug']) ? $movie['slug'] : '#';
+
                         $img_src = !empty($movie['affiche_url']) ? base_url($movie['affiche_url']) : base_url('assets/img/full.jpg');
                         ?>
                         <div class="col-md-3 col-6 mb-3">
@@ -21,7 +21,7 @@ $controller = strtolower(basename(str_replace('\\', '/', $router->controllerName
                                 <img src="<?= $img_src ?>" class="card-img-top" alt="<?= ($title); ?>">
                                 <div class="card-body">
                                     <div class="card-title">
-                                        <a href="<?= base_url('film/' . $slug) ?>"><?= ($title); ?></a>
+                                        <a href="<?= base_url('film/' . $movie['slug']) ?>"><?= ($title); ?></a>
                                     </div>
                                 </div>
                             </div>
